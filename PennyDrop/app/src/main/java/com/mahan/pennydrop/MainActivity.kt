cbe323a937e6e6b9cb2901e1be4253b915525e8b
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-12-11 11:04:09 +0330

    Add ViewBinding and updated MainActivity with navigation

--- PennyDrop/app/src/main/java/com/mahan/pennydrop/MainActivity.kt
+++ PennyDrop/app/src/main/java/com/mahan/pennydrop/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.mahan.pennydrop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.mahan.pennydrop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        this.navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
